05cfcb6ac4dfe8f5a79e8e12cde5352e05c47605
Ozcan <ocucan23023@gmail.com>
2025-12-06 01:24:37 +0300

    Altın a göre bina ekleme işlemi tamamlandı

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public int populationBonus = 3;  // Bu bina population cap'i ne kadar yükseltecek?
+    public int goldCost = 10;        // Binanın maliyeti
+
+    void Start()
+    {
+        // Bina kurulduğu anda population cap'i artır
+        ResourceManager.Instance.AddPopulationCap(populationBonus);
+    }
+}
+    

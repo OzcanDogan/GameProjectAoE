0e2706246efb595de9a00a14da88e399544ae93b
Ozcan <ocucan23023@gmail.com>
2025-12-08 15:51:59 +0300

    adding photon to buildings

---
+++ Assets/Scripts/UnitHealth.cs
@@ -0,0 +1,45 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class UnitHealth : MonoBehaviourPun
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    public bool isBuilding = false; // Bina mı?
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    [PunRPC]
+    public void TakeDamage(int amount)
+    {
+        if (currentHealth <= 0) return;
+
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log($"{gameObject.name} öldü.");
+
+        if (isBuilding)
+        {
+            // Population bonus geri alınır
+            Building building = GetComponent<Building>();
+            if (building != null)
+                ResourceManager.Instance.AddPopulationCap(-building.populationBonus);
+        }
+
+        PhotonNetwork.Destroy(gameObject);
+    }
+
+    public int GetHealth() => currentHealth;
+}
